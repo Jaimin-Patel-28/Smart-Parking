65ab14ae9f2d1b1665701fddb85c159a3855fc42
Jaimin-Patel-28 <pj64530@gmail.com>
2026-03-10 12:55:11 +0530

    pass routes

--- Frontend/src/app/Routes/AdminRoutes.jsx
+++ Frontend/src/app/Routes/AdminRoutes.jsx
@@ -1,6 +1,10 @@
 import { Route } from "react-router-dom";
 import AdminLayout from "../../admin-panel/layout/AdminLayout";
 
+import Dashboard from "../../admin-panel/Pages/Dashboard";
+import ParkingManagement from "../../admin-panel/Pages/ParkingManager";
+import ParkingManager from "../../admin-panel/Pages/ParkingManager";
+
 const Placeholder = ({ title }) => (
   <h1 className="text-2xl font-bold">{title}</h1>
 );
@@ -8,14 +12,11 @@ const Placeholder = ({ title }) => (
 const AdminRoutes = () => {
   return (
     <Route path="admin" element={<AdminLayout />}>
-      <Route
-        path="dashboard"
-        element={<Placeholder title="Dashboard Page" />}
-      />
+      <Route path="dashboard" element={<Dashboard />} />
 
       <Route 
       path="parking" 
-      element={<Placeholder title="Parking Page" />} 
+      element={<ParkingManager />} 
       />
 
       <Route 
